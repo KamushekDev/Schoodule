78708c341df6eabb8a1f70213cf705a4f7f9ed46
Kirill Ilin <kamushek.stone@gmail.com>
2021-03-13 15:53:25 +0300

    TEst change

--- Schoodule.API/Startup.cs
+++ Schoodule.API/Startup.cs
@@ -83,10 +83,8 @@ namespace Schoodule.API
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
+			app.UseDeveloperExceptionPage();
+			if (env.IsDevelopment()) { }
 
 			app.UseSwagger();
 			app.UseSwaggerUI(options => { options.SwaggerEndpoint("/swagger/v1/swagger.json", "Schoodule v1"); });
